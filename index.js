ccaa2da60740221d281d3e1fd1cb6b035934acbf
Roger Steve Ruiz <hi@rog.gr>
2016-11-07 00:08:05 -0500

    Add custom log support for gource and code_swarm

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const currentRepo = require( 'simple-git' )( '.' )
 const mori = require( 'mori' )
 const getLog = require( './lib/get-log' )
 const getFiles = require( './lib/get-files' )
+const customLog = require( './lib/custom-log' )
 
 getLog( currentRepo )
   .then( function ( data ) {
@@ -11,7 +12,12 @@ getLog( currentRepo )
     return Promise.all( mori.intoArray( pendingFiles ) )
   } )
   .then( function ( commits ) {
-    console.log( commits )
+    var buildLines = customLog( commits, 'code_swarm' )
+    return Promise.all( mori.intoArray( buildLines ) )
+  } )
+  .then( function ( lines ) {
+    lines = mori.flatten( lines )
+    console.log( mori.intoArray( lines ).join( '\n' ) )
   } )
   .catch( function ( error ) {
     console.error( error )
